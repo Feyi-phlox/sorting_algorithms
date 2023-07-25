f73af41eaec69e6ebc90573afb6583fd62c5aea3
Feyi-phlox <feyisayoakinbobola@gmail.com>
2023-07-25 09:07:05 -0700

    insertion sorting algorithm

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -20,7 +20,7 @@ void insertion_sort_list(listint_t **list)
 	while (current != NULL)
 	{
 
-		while (temp->prev != NULL && temp->n < temp->prev->n)
+		while (current->prev != NULL && (current->n < current->prev->n)
 		{
 			/* Swap nodes */
 			temp = current->next;
